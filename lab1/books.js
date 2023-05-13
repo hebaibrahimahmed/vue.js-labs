1a28cc8a2e6d254e9e182c5ecd57a15ee88af8b0
unknown <hebaalhwai96@gmail.com>
2023-05-13 23:14:47 +0300

    lab1 and lab2

---
+++ lab1/books.js
@@ -0,0 +1,90 @@
+books=[
+        { 
+          "id": "001",
+          "isbn":"99921-58-10-7",
+          "author": "Dante Alighieri",
+          "imageLink": "images/02b046b95473893247bdfa857ebb0bcb.jpg",
+          "category": "romans",
+          "pages": 209,
+          "quantity":1,
+          "title": "Things Fall Apart",
+          "price": 60
+        },
+        {
+          "id": "002",
+          "isbn":"99921-58-15-8",
+          "author": "Hans Christian",
+          "imageLink": "images/0d2581ed1bc8983b7c1c31a41124f192.jpg",
+          "category": "novels",
+          "pages": 784,
+          "quantity":1,
+          "title": "Fairy tales",
+          "price": 55
+        },
+        {
+          "id": "003",
+          "author": "Dante Alighieri",
+          "isbn":"99921-58-12-3",
+          "imageLink": "images/1a97084cf657c750b2527290ec3fe2a9.jpg",
+          "category": "stories",
+          "pages": 928,
+          "quantity":1,
+          "title": "The Divine Comedy",
+          "price": 50
+        },
+        {
+          "id": "004",
+          "isbn":"99921-58-11-6",
+          "author": "Andersen ",
+          "imageLink": "images/46a60e98019aace07419935c1cab4713.jpg",
+          "category": "stories",
+          "pages": 160,
+          "quantity":1,
+          "title": "Gilgamesh",
+          "price": 20
+        },
+        {
+          "id": "005",
+          "isbn":"93-86954-21-4",
+          "author": "Achaemenid",
+          "imageLink": "images/23eda7253233449a8fc57d0daedce603.jpg",
+          "category": "programming",
+          "pages": 176,
+          "title": "The Book Of Job",
+          "price": 90
+        },
+        {
+          "id": "006",
+          "isbn":"99921-58-13-8",
+          "author": "tomy ",
+          "imageLink": "images/8bae1a37c088f06ce19b89f10a5b0707.jpg",
+          "category": "history",
+          "pages": 288,
+          "quantity":1,
+          "title": "One Thousand and One Nights",
+          "price": 80
+        },
+        {
+          "id": "007",
+          "isbn":"99921-58-18-6",
+          "author": "Andersen",
+          "imageLink": "images/23eda7253233449a8fc57d0daedce603.jpg",
+          "category": "languages",
+          "pages": 384,
+          "quantity":1,
+          "title": "Nj\u00e1l's Saga",
+          "price": 35
+        },
+        {
+          "id": "008",
+          "isbn":"90-684-84328-5",
+          "author": "Austen",
+          "imageLink": "images/7ac65545c1ca9237d2527a0b7be119d5.jpg",
+          "category": "Art",
+          "pages": 226,
+          "quantity":1,
+          "title": "Pride",
+          "price": 40
+        },
+       
+        ]
